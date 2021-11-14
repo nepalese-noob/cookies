9c47b7950bb40536af1322850643989e7a2a04bd
nepalese-noob <85428811+nepalese-noob@users.noreply.github.com>
2021-11-14 18:30:20 +0545

    Create aide.sh

---
+++ aide.sh
@@ -0,0 +1,27 @@
+#!bin/bash
+echo "you have to download Aide modded virsion from the link i am giving"
+echo "choose chrome after 5 second"
+clear
+echo "you have to download Aide modded virsion from the link i am giving"
+echo "choose chrome after 4 second"
+clear
+echo "you have to download Aide modded virsion from the link i am giving"
+echo "choose chrome after 3 second"
+clear
+echo "you have to download Aide modded virsion from the link i am giving"
+echo "choose chrome after 2 second"
+clear
+echo "you have to download Aide modded virsion from the link i am giving"
+echo "choose chrome after 1 second"
+clear
+echo "lets go..."
+xdg-open https://drive.google.com/file/d/1U_7oGMCxvjOrqDhoKN0dW4YJ8OblVSCq/view?usp=drivesdk
+clear
+echo "you downloaded aide, good🥰🥰🥰"
+sleep 5
+clear
+echo "now i am going to clone facebook apk git tool...5"
+echo "now i am going to clone facebook apk git tool...4"
+echo "now i am going to clone facebook apk git tool...3"
+echo "now i am going to clone facebook apk git tool...2"
+echo "now i am going to clone facebook apk git tool...1"
